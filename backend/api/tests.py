4fa1c534ddfd7ac394426274c3e2b2c43e5d2830
Tatiana136 <154363892+Tatiana136@users.noreply.github.com>
2025-01-20 21:05:03 +0300

    Add docker build2

---
+++ backend/api/tests.py
@@ -0,0 +1,13 @@
+from http import HTTPStatus
+
+from api import models
+from django.test import Client, TestCase
+
+class TaskiAPITestCase(TestCase):
+    def setUp(self):
+        self.guest_client = Client()
+
+    def test_list_exists(self):
+        """Проверка доступности рецептов."""
+        response = self.guest_client.get('/api/recipes/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
